0116f5821e8c54e8a1c5488e19f7ca858fd8f286
hamidds <hamids9965@gmail.com>
2022-02-16 18:26:13 +0330

    fix: change ctc loss

--- losses/CTC_loss.py
+++ losses/CTC_loss.py
@@ -10,6 +10,6 @@ def compute_ctc_loss(y_preds, y_train):
     input_length = torch.IntTensor(batch).fill_(T)
     target_length = torch.IntTensor([len(t) for t in y_train])
 
-    loss = ctc_loss(y_preds, y_train, input_length, target_length, reduction='sum')
+    loss = ctc_loss(y_preds, y_train, input_length, target_length, reduction='sum', zero_infinity=True)
 
     return loss
