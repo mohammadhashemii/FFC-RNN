81543420b5224765774545d8229a12acabee9d7b
hamidds <hamids9965@gmail.com>
2022-02-19 11:26:34 +0330

    fix: change FFC

--- models/FFC.py
+++ models/FFC.py
@@ -1,171 +1,202 @@
-import argparse
-import os
-import json
-
 import torch
-from torch.utils.data import DataLoader
-from dataset import SadriDataset
-from tqdm import tqdm
-from models.FFCRnn import FFCRnn
-from models.FFCResnet import *
-from losses.CTC_loss import compute_ctc_loss
-from torchsummary import summary
-from utils import ctc_decode
-import torch.nn.functional as F
+import torch.nn as nn
+# import FFCResnet
 
-parser = argparse.ArgumentParser()
-# directories
-parser.add_argument('--data_root', default='data', help='path to dataset directory')
-parser.add_argument('--exp_dir', default='experiments', help='path to experiments directory')
-# training
-parser.add_argument('--exp', required=True, type=str, help='experiments number e.g. 01')
-parser.add_argument('--resume', type=str, default=None, help='path to the checkpoint')
-parser.add_argument('--n_epochs', type=int, default=100, help='number of epochs for training')
-parser.add_argument('--batch_size', type=int, default=128, help='batch size')
-parser.add_argument('--imgH', type=int, default=32, help='input image height')
-parser.add_argument('--imgW', type=int, default=256, help='input image width')
-parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate')
-parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
-parser.add_argument('--num_workers', type=int, default=1, help='number of workers')
-args = parser.parse_args()
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-if not torch.cuda.is_available():
-    print("WARNING: You are not using gpu!")
+class FfcseBlock(nn.Module):
 
-if not os.path.exists(os.path.join(args.exp_dir, args.exp)):
-    os.makedirs(os.path.join(args.exp_dir, args.exp))
+    def __init__(self, channels, ratio_g):
+        super(FfcseBlock, self).__init__()
+        in_cg = int(channels * ratio_g)
+        in_cl = channels - in_cg
+        r = 16
 
-# load data and create data loaders
-img_size = (args.imgW, args.imgH)
-train_dataset = SadriDataset(root_dir=args.data_root, img_size=img_size, is_training_set=True)
-test_dataset = SadriDataset(root_dir=args.data_root, img_size=img_size, is_training_set=False)
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.conv1 = nn.Conv2d(channels, channels // r, kernel_size=(1, 1), bias=True)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv_a2l = None if in_cl == 0 else nn.Conv2d(channels // r, in_cl, kernel_size=(1, 1), bias=True)
+        self.conv_a2g = None if in_cg == 0 else nn.Conv2d(channels // r, in_cg, kernel_size=(1, 1), bias=True)
+        self.sigmoid = nn.Sigmoid()
 
-loader_args = dict(batch_size=args.batch_size,
-                   num_workers=args.num_workers,
-                   pin_memory=True)
-train_loader = DataLoader(train_dataset, shuffle=False, **loader_args)
-test_loader = DataLoader(test_dataset, shuffle=False, **loader_args)
-n_train = len(train_dataset)
+    def forward(self, x):
+        x = x if type(x) is tuple else (x, 0)
+        id_l, id_g = x
 
-# create the model
-ffc_rnn = FFCRnn(image_height=args.imgH,
-                 nc=1,  # since the images are black and white
-                 nh=256,
-                 output_number=len(train_dataset.wv.word_vocab) + 1,
-                 n_rnn=4,
-                 leaky_relu=False,
-                 map_to_seq_hidden=512,
-                 feature_extractor=ffc_resnet18())
+        x = id_l if type(id_g) is int else torch.cat([id_l, id_g], dim=1)
+        x = self.avg_pool(x)
+        x = self.relu1(self.conv1(x))
 
-ffc_rnn.to(device=device)
-if args.resume is not None:
-    ffc_rnn.load_state_dict(torch.load(os.path.join(args.exp_dir, args.exp, args.resume)))
-    print(f"Model weights {os.path.join(args.exp_dir, args.exp, args.resume)} loaded!")
-
-# set up optimizer, learning rate, etc.
-optimizer = torch.optim.Adam(ffc_rnn.parameters(),
-                             lr=args.learning_rate)
-
-# Initialize logging
-training_configs = dict(exp=args.exp, epochs=args.n_epochs,
-                        batch_size=args.batch_size, learning_rate=args.learning_rate,
-                        device=device.type)
-json_path = os.path.join(args.exp_dir, args.exp, f"training_configs.json")
-with open(json_path, "w") as jf:
-    json.dump(training_configs, jf)
-print(f"Training configs:\n {training_configs}")
-
-#model_summary = summary(ffc_rnn, (1, args.imgH, args.imgW), batch_size=-1, device='cuda')
-#model_summary_path = os.path.join(args.exp_dir, args.exp, f"model_summary.txt")
-#with open(model_summary, "w") as f:
-#    f.write(model_summary)
-#    f.close()
-
-# training
-epoch_train_loss_list = []
-epoch_val_loss_list = []
-least_loss = None
-for epoch in range(args.n_epochs):
-    ############## Training ##############
-    ffc_rnn.train()
-    tot_correct = 0
-    wrong_cases = []
-    with tqdm(total=n_train, desc=f"Epoch {epoch + 1}/{args.n_epochs}", unit='img') as pbar:
-        for batch in train_loader:
-            images = batch['image'].to(device=device, dtype=torch.float32)  # (batch, c=1, imgH, imgW)
-            labels = batch['label'].to(device=device, dtype=torch.int)
-
-            preds = ffc_rnn(images)
-            log_probs = F.log_softmax(preds, dim=2)
-            loss = compute_ctc_loss(log_probs, labels) / images.size()[0]
-            epoch_train_loss_list.append(loss)
-            '''
-            decoded_preds, indices_list = ctc_decode(log_probs, 0, SadriDataset, training=True)
-            target_lengths = torch.IntTensor([len(list(filter(lambda a: a != 0, t))) for t in labels])
-            target_length_counter = 0
-            for i in range(len(labels)):
-                label_indices = labels[i]
-                l = SadriDataset.wv.num_to_word(label_indices)
-                words = [w for w in l if w != '<unk>']
-                ground_truth_sentence = " ".join(words)
-                if ground_truth_sentence == decoded_preds[i]:
-                    tot_correct += 1
-                else:
-                    wrong_cases.append((ground_truth_sentence, decoded_preds[i]))
-            '''
-            ffc_rnn.zero_grad()
-            loss.backward()
-            optimizer.step()
-            pbar.update(images.shape[0])
-        avg_train_loss = torch.mean(torch.tensor(epoch_train_loss_list))
-        train_accuracy = tot_correct / train_dataset.num_samples
-
-        ############## validation ##############
-        ffc_rnn.eval()
-        tot_correct = 0
-        wrong_cases = []
-        for batch in test_loader:
-            images = batch['image'].to(device=device, dtype=torch.float32)
-            labels = batch['label'].to(device=device, dtype=torch.int)
-            with torch.no_grad():
-                preds = ffc_rnn(images)
-
-                log_probs = F.log_softmax(preds, dim=2)
-                val_loss = compute_ctc_loss(log_probs, labels).item() // images.size(0)
-                epoch_val_loss_list.append(val_loss)
-
-                decoded_preds, indices_list = ctc_decode(log_probs, 0, SadriDataset, training=False)
-                # target_lengths = torch.IntTensor([len(t) for t in labels])
-                target_lengths = torch.IntTensor([len(list(filter(lambda a: a != 0, t))) for t in labels])
-                target_length_counter = 0
-                for i in range(len(labels)):
-                    label_indices = labels[i]
-                    l = SadriDataset.wv.num_to_word(label_indices)
-                    words = [w for w in l if w != '<unk>']
-                    ground_truth_sentence = " ".join(words)
-                    if ground_truth_sentence == decoded_preds[i]:
-                        tot_correct += 1
-                    else:
-                        wrong_cases.append((ground_truth_sentence, decoded_preds[i]))
-
-        avg_val_loss = torch.mean(torch.tensor(epoch_val_loss_list))
-        val_accuracy = tot_correct / test_dataset.num_samples
-
-        # log losses and ... in a text file
-        log_file_path = os.path.join(args.exp_dir, args.exp, f"training.log")
-        with open(log_file_path, "a") as f:
-            msg = f"Train loss: {avg_train_loss}, Train acc:{train_accuracy}, Test loss:{avg_val_loss}, Val acc:{val_accuracy}\n"
-            print(msg)
-            f.write(msg)
-        f.close()
-
-        # do checkpointing
-        if not os.path.exists(os.path.join(args.exp_dir, args.exp, 'checkpoints')):
-            os.makedirs(os.path.join(args.exp_dir, args.exp, 'checkpoints'))
-        if least_loss is None or avg_val_loss < least_loss:
-            checkpoint_path = os.path.join(args.exp_dir, args.exp, 'checkpoints', f"checkpoint_best")
-            torch.save(ffc_rnn.state_dict(), checkpoint_path)
-            least_loss = avg_val_loss
+        x_l = 0 if self.conv_a2l is None else id_l * self.sigmoid(self.conv_a2l(x))
+        x_g = 0 if self.conv_a2g is None else id_g * self.sigmoid(self.conv_a2g(x))
+        return x_l, x_g
 
 
+class FourierUnit(nn.Module):
 
+    def __init__(self, in_channels, out_channels, groups=1):
+        # bn_layer not used
+        super(FourierUnit, self).__init__()
+        self.groups = groups
+        self.conv_layer = torch.nn.Conv2d(in_channels=in_channels * 2, out_channels=out_channels * 2,
+                                          kernel_size=(1, 1), stride=(1, 1), padding=0, groups=self.groups, bias=False)
+        self.bn = torch.nn.BatchNorm2d(out_channels * 2)
+        self.relu = torch.nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        batch, c, h, w = x.size()
+        # print(x.size())
+        # fft_dim = (-3, -2, -1) if self.ffc3d else (-2, -1)
+        fft_dim = (-2, -1)
+        ffted = torch.fft.rfftn(x, dim=fft_dim)
+        ffted = torch.stack((ffted.real, ffted.imag), dim=-1)
+        ffted = ffted.permute(0, 1, 4, 2, 3).contiguous()  # (batch, c, 2, h, w/2+1)
+        ffted = ffted.view((batch, -1,) + ffted.size()[3:])
+
+        # print(ffted.size())
+
+        ffted = self.conv_layer(ffted)  # (batch, c*2, h, w/2+1)
+        ffted = self.relu(self.bn(ffted))
+
+        ffted = ffted.view((batch, -1, 2,) + ffted.size()[2:]).permute(0, 1, 3, 4,
+                                                                       2).contiguous()  # (batch,c, t, h, w/2+1, 2)
+        ffted = torch.complex(ffted[..., 0], ffted[..., 1])
+        # ifft_shape_slice = x.shape[-3:] if self.ffc3d else x.shape[-2:]
+        ifft_shape_slice = x.shape[-2:]
+        output = torch.fft.irfftn(ffted, s=ifft_shape_slice, dim=fft_dim)
+
+        return output
+
+
+class SpectralTransform(nn.Module):
+
+    def __init__(self, in_channels, out_channels, stride=1, groups=1, enable_lfu=True):
+        # bn_layer not used
+        super(SpectralTransform, self).__init__()
+        self.enable_lfu = enable_lfu
+
+        if stride == 2:
+            self.down_sample = nn.AvgPool2d(kernel_size=(2, 2), stride=2)
+        else:
+            self.down_sample = nn.Identity()
+
+        self.stride = stride
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels // 2, kernel_size=(1, 1), groups=groups, bias=False),
+            nn.BatchNorm2d(out_channels // 2),
+            nn.ReLU(inplace=True)
+        )
+
+        self.fu = FourierUnit(out_channels // 2, out_channels // 2, groups)
+
+        if self.enable_lfu:
+            self.lfu = FourierUnit(out_channels // 2, out_channels // 2, groups)
+
+        self.conv2 = torch.nn.Conv2d(out_channels // 2, out_channels, kernel_size=(1, 1), groups=groups, bias=False)
+
+    def forward(self, x):
+        # print("input x size at SpectralTransform", x.size())
+        x = self.down_sample(x)
+        x = self.conv1(x)
+
+        output = self.fu(x)
+
+        if self.enable_lfu:
+            n, c, h, w = x.shape
+            # print("lfu x size at SpectralTransform", x.shape)
+            split_no = 2
+            split_s = h // split_no
+            split_s_w = w // split_no
+            xs = torch.cat(torch.split(x[:, :c // 4], split_s, dim=-2), dim=1).contiguous()
+            # xs = torch.cat(torch.split(xs, split_s, dim=-1), dim=1).contiguous()
+            xs = torch.cat(torch.split(xs, split_s_w, dim=-1), dim=1).contiguous()
+            # print("xs size at SpectralTransform", xs.size())
+            xs = self.lfu(xs)
+            xs = xs.repeat(1, 1, split_no, split_no).contiguous()
+        else:
+            xs = 0
+
+        output = self.conv2(x + output + xs)
+
+        return output
+
+
+class FFC(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size,
+                 ratio_gin, ratio_gout, stride=1, padding=0,
+                 dilation=1, groups=1, bias=False, enable_lfu=True):
+        super(FFC, self).__init__()
+
+        assert stride == 1 or stride == 2, "Stride should be 1 or 2."
+        self.stride = stride
+
+        in_cg = int(in_channels * ratio_gin)
+        in_cl = in_channels - in_cg
+        out_cg = int(out_channels * ratio_gout)
+        out_cl = out_channels - out_cg
+
+        # print("in_cg", in_cg)
+        # print("in_cl", in_cl)
+        # print("out_cg", out_cg)
+        # print("out_cl", out_cl)
+        # groups_g = 1 if groups == 1 else int(groups * ratio_gout)
+        # groups_l = 1 if groups == 1 else groups - groups_g
+
+        self.ratio_gin = ratio_gin
+        self.ratio_gout = ratio_gout
+
+        module = nn.Identity if in_cl == 0 or out_cl == 0 else nn.Conv2d
+        self.conv_l2l = module(in_cl, out_cl, kernel_size, stride, padding, dilation, groups, bias)
+
+        module = nn.Identity if in_cl == 0 or out_cg == 0 else nn.Conv2d
+        self.conv_l2g = module(in_cl, out_cg, kernel_size, stride, padding, dilation, groups, bias)
+
+        module = nn.Identity if in_cg == 0 or out_cl == 0 else nn.Conv2d
+        self.conv_g2l = module(in_cg, out_cl, kernel_size, stride, padding, dilation, groups, bias)
+
+        module = nn.Identity if in_cg == 0 or out_cg == 0 else SpectralTransform
+        self.conv_g2g = module(in_cg, out_cg, stride, 1 if groups == 1 else groups // 2, enable_lfu)
+        # self.conv_g2g = module(in_cg, out_cg, stride, 1 if groups == 1 else groups // 2, False)
+
+    def forward(self, x):
+        x_l, x_g = x if type(x) is tuple else (x, 0)
+        out_xl, out_xg = 0, 0
+
+        if self.ratio_gout != 1:
+            out_xl = self.conv_l2l(x_l) + self.conv_g2l(x_g)
+        if self.ratio_gout != 0:
+            out_xg = self.conv_l2g(x_l) + self.conv_g2g(x_g)
+
+        return out_xl, out_xg
+
+
+class FfcBnAct(nn.Module):
+
+    def __init__(self, in_channels, out_channels,
+                 kernel_size, ratio_gin, ratio_gout,
+                 stride=1, padding=0, dilation=1, groups=1, bias=False,
+                 norm_layer=nn.BatchNorm2d, activation_layer=nn.Identity,
+                 enable_lfu=True):
+        super(FfcBnAct, self).__init__()
+
+        self.ffc = FFC(in_channels, out_channels, kernel_size,
+                       ratio_gin, ratio_gout, stride, padding, dilation,
+                       groups, bias, enable_lfu)
+
+        l_norm = nn.Identity if ratio_gout == 1 else norm_layer
+        g_norm = nn.Identity if ratio_gout == 0 else norm_layer
+
+        self.bn_l = l_norm(int(out_channels * (1 - ratio_gout)))
+        self.bn_g = g_norm(int(out_channels * ratio_gout))
+
+        l_act = nn.Identity if ratio_gout == 1 else activation_layer
+        g_act = nn.Identity if ratio_gout == 0 else activation_layer
+
+        self.act_l = l_act(inplace=True)
+        self.act_g = g_act(inplace=True)
+
+    def forward(self, x):
+        x_l, x_g = self.ffc(x)
+        x_l = self.act_l(self.bn_l(x_l))
+        x_g = self.act_g(self.bn_g(x_g))
+
+        return x_l, x_g
