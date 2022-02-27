037f6695eb82bdbe533a92ac1b90a4b664720ac9
hamidds <hamids9965@gmail.com>
2022-02-27 12:28:53 +0330

    fix: change wer

--- metric.py
+++ metric.py
@@ -10,11 +10,11 @@ class HandwrittenRecognitionMetrics:
 
     def update_metric(self, sample: str, ground_truth: str):
         measures = compute_measures(ground_truth, sample)
-        word_errors = self.wer * self.n_words + (measures.substitutions + measures.deletions + measures.insertions)
+        word_errors = self.wer * self.n_words + (
+                    measures['substitutions'] + measures['deletions'] + measures['insertions'])
         ser_sum = self.ser * self.n_smaples + int(ground_truth != sample)
 
         self.n_smaples += 1
         self.n_words += len(ground_truth.split(" "))
         self.wer = word_errors / self.n_words
         self.ser = ser_sum / self.n_smaples
-
