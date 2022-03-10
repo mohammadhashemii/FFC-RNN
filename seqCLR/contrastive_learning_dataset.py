593da6b6c3e5fc88f53ac7d9306e9b95337415a7
hamidds <hamids9965@gmail.com>
2022-03-11 01:41:00 +0330

    fix: change contrastive_learning_dataset

--- seqCLR/contrastive_learning_dataset.py
+++ seqCLR/contrastive_learning_dataset.py
@@ -2,15 +2,55 @@ import os
 import torchvision
 from torchvision.transforms import transforms
 from torch.utils.data import Dataset
-from transformations import *
+from seqCLR.transformations import *
 import copy
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def distortion_free_resize(img, img_size):
+    w, h = img_size
+    old_w, old_h = img.shape[2], img.shape[1]
+    new_size = h if old_w >= old_h else w
+
+    img = torchvision.transforms.Compose([
+        torchvision.transforms.Grayscale(),
+        torchvision.transforms.Resize(new_size),
+
+    ])(img)
+
+    # check the amount of padding needed to be done
+    pad_height = h - img.shape[1]
+    pad_width = w - img.shape[2]
+
+    # only necessary if you want to do same amount of padding on both sides
+    if pad_height % 2 != 0:
+        height = pad_height // 2
+        pad_height_top = height + 1
+        pad_height_bottom = height
+    else:
+        pad_height_top = pad_height_bottom = pad_height // 2
+
+    if pad_width % 2 != 0:
+        width = pad_width // 2
+        pad_width_left = width + 1
+        pad_width_right = width
+    else:
+        pad_width_left = pad_width_right = pad_width // 2
+
+    img = torchvision.transforms.Compose([
+        torchvision.transforms.Pad([pad_width_left, pad_height_top,
+                                    pad_width_right, pad_height_bottom]),
+
+    ])(img)
+
+    return img
+
+
 class PersianHandwrittenStringsDataset(Dataset):
     def __init__(self, data_dir, name, transform=None):
         self.data_dir = data_dir
-        self.images_dir = os.path.join(data_dir, name, 'images')
+        self.images_dir = os.path.join(data_dir, 'images')
         self.name = name
         self.filenames = os.listdir(self.images_dir)
         self.transform = transform
@@ -22,8 +62,9 @@ class PersianHandwrittenStringsDataset(Dataset):
         img_filename = self.filenames[idx]
         img_path = os.path.join(self.images_dir, img_filename)
         img = torchvision.io.read_image(str(img_path))
+        img = distortion_free_resize(img, img_size=(256, 32))
         img = transforms.Compose([transforms.ToPILImage(),
-                                  transforms.Grayscale()])(img)
+                                  ])(img)
         img = self.transform(img)
 
         return img
@@ -48,13 +89,13 @@ class ContrastiveLearningDataset:
     @staticmethod
     def get_seqclr_pipeline_transform(img_size):
         linear_contrast = transforms.ColorJitter(0, 0.8, 0, 0)
-        data_transforms = transforms.Compose([transforms.GaussianBlur(kernel_size=int(0.1*img_size[1])),
+        data_transforms = transforms.Compose([transforms.GaussianBlur(kernel_size=int(0.1 * img_size[1])),
                                               transforms.RandomApply([linear_contrast], p=0.8),
                                               transforms.RandomAdjustSharpness(2),
                                               transforms.RandomAffine(degrees=(-3, 3), shear=(0.8, 1), fill=255),
                                               transforms.ToTensor(),
-                                              AddGaussianNoise(0, 0.05)])
-
+                                              AddGaussianNoise(0, 0.05)
+                                              ])
         return data_transforms
 
     def get_dataset(self, name, n_views):
@@ -62,12 +103,14 @@ class ContrastiveLearningDataset:
             'SadriDataset': lambda: PersianHandwrittenStringsDataset(data_dir=self.data_dir,
                                                                      name=name,
                                                                      transform=ContrastiveLearningViewGenerator(
-                                                                         self.get_seqclr_pipeline_transform(img_size=self.img_size),
+                                                                         self.get_seqclr_pipeline_transform(
+                                                                             img_size=self.img_size),
                                                                          n_views=n_views)),
             'OurDataset': lambda: PersianHandwrittenStringsDataset(data_dir=self.data_dir,
                                                                    name=name,
                                                                    transform=ContrastiveLearningViewGenerator(
-                                                                       self.get_seqclr_pipeline_transform(img_size=self.img_size),
+                                                                       self.get_seqclr_pipeline_transform(
+                                                                           img_size=self.img_size),
                                                                        n_views=n_views
                                                                    ))
         }
@@ -98,9 +141,8 @@ def visualize_samples(dataset, n_samples=8, cols=4, random_img=False):
     plt.tight_layout(pad=1)
     plt.show()
 
-
-#c = PersianHandwrittenStringsDataset(data_dir="data", name="OurDataset")
-#dataset = ContrastiveLearningDataset(data_dir="data", img_size=(256, 32))
-#train_dataset = dataset.get_dataset('SadriDataset', 1)
-#print(train_dataset[0][0].shape)
-#visualize_samples(train_dataset, random_img=True, n_samples=20)
+# c = PersianHandwrittenStringsDataset(data_dir="data", name="OurDataset")
+# dataset = ContrastiveLearningDataset(data_dir="data", img_size=(256, 32))
+# train_dataset = dataset.get_dataset('SadriDataset', 1)
+# print(train_dataset[0][0].shape)
+# visualize_samples(train_dataset, random_img=True, n_samples=20)
